b13d6b386eb3bbaa07330b20245bffdb6daa8395
PavelETU <pavelsuvit@gmail.com>
2020-04-27 10:45:25 +0100

    Initial Commit

---
+++ Covid 19 Cases Lookup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid 19 Cases Lookup
+//
+//  Created by Pavel Suvit on 27/04/2020.
+//  Copyright © 2020 Pavel Suvit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
