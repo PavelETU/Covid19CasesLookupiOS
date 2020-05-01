a5d7ae1f402eeedd54557f60453ab9bf4e3347a6
PavelETU <pavelsuvit@gmail.com>
2020-05-01 17:49:07 +0100

    Propagate errors from repository to outer layer by calling completionCallback with nil value

--- Covid 19 Cases Lookup/CasesLookupRepositoryImpl.swift
+++ Covid 19 Cases Lookup/CasesLookupRepositoryImpl.swift
@@ -12,16 +12,23 @@ class CasesLookupRepositoryImpl: CasesLookupRepository {
     func loadCountries(completionCallback: @escaping ([Country]?) -> ()) {
         let url = URL(string: "https://api.covid19api.com/countries")!
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                self.handleClientError(error: error)
+            if error != nil {
+                DispatchQueue.main.async {
+                    completionCallback(nil)
+                }
                 return
             }
             guard let httpResponse = response as? HTTPURLResponse,
                 (200...299).contains(httpResponse.statusCode) else {
-                    self.handleServerError(errorResponse: response!)
+                    DispatchQueue.main.async {
+                        completionCallback(nil)
+                    }
                     return
             }
             guard let countriesArray = try? JSONDecoder().decode([Country].self, from: data!) else {
+                DispatchQueue.main.async {
+                    completionCallback(nil)
+                }
                 return
             }
             DispatchQueue.main.async {
@@ -29,13 +36,5 @@ class CasesLookupRepositoryImpl: CasesLookupRepository {
             }
         }
         task.resume()
-    }
-    
-    private func handleClientError(error: Error) {
-        
-    }
-    
-    private func handleServerError(errorResponse: URLResponse) {
-        
     }
 }
