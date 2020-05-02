34bcc0605e0e86a053282add4abac7233d745eeb
PavelETU <pavelsuvit@gmail.com>
2020-05-02 15:55:31 +0100

    Create folders for data and presentation layers, move corresponding files

---
+++ Covid 19 Cases Lookup/Data/Entities/StatsForCountry.swift
@@ -0,0 +1,9 @@
+//
+//  StatsForCountry.swift
+//  Covid 19 Cases Lookup
+//
+//  Created by Pavel Suvit on 02/05/2020.
+//  Copyright © 2020 Pavel Suvit. All rights reserved.
+//
+
+import Foundation
